2f97e932747ea48b5c730e960bff6372dd984aeb
zou <zoujingyi1992@gmail.com>
2023-07-21 10:02:04 +0800

    first commit

---
+++ module_wallet/src/main/java/com/ty/module_wallet/fragment/WalletSignFragment.kt
@@ -0,0 +1,208 @@
+package com.ty.module_sign.fragment
+
+import android.content.Context
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
+import androidx.constraintlayout.widget.ConstraintLayout
+import com.bumptech.glide.Glide
+import com.google.android.material.bottomsheet.BottomSheetDialog
+import com.ty.dappconnect.DAppConnect
+import com.ty.module_sign.interfaces.OnConnectCallback
+import com.ty.module_sign.interfaces.OnSignCallback
+import com.ty.module_sign.interfaces.WalletInitCallback
+import com.ty.module_wallet.R
+import com.ty.module_wallet.fragment.BaseFragment
+import com.ty.web3mq.Web3MQClient
+import com.ty.web3mq.interfaces.BridgeConnectCallback
+import com.ty.web3mq.interfaces.ConnectCallback
+import com.ty.web3mq.interfaces.SendBridgeMessageCallback
+import com.ty.web3mq.websocket.bean.BridgeMessageMetadata
+import com.ty.web3mq.websocket.bean.sign.Participant
+
+class WalletSignFragment : BaseFragment(), ConnectCallback {
+
+    private var bottomSheetDialog: BottomSheetDialog? = null
+    private var connectCallback: OnConnectCallback? = null
+    private var onSignCallback: OnSignCallback? = null
+    private var dAppID: String? = null
+    private var topicId: String? = null
+    private var pubKey: String? = null
+    private var callback: WalletInitCallback? = null
+    private lateinit var cl_reconnect: ConstraintLayout
+    private lateinit var btn_reconnect: Button
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent(R.layout.fragment_wallet_sign)
+    }
+
+    override fun onBaseCreateView() {
+        super.onBaseCreateView()
+        cl_reconnect = rootView.findViewById(R.id.cl_reconnect)
+        btn_reconnect = rootView.findViewById(R.id.btn_reconnect)
+        btn_reconnect.setOnClickListener {
+            Web3MQClient.startConnect(this)
+            cl_reconnect.visibility = View.GONE
+        }
+    }
+
+    fun init(dAppID: String, topicId: String, pubKey: String, callback: WalletInitCallback) {
+        this.dAppID = dAppID
+        this.topicId = topicId
+        this.pubKey = pubKey
+        this.callback = callback
+        Web3MQClient.startConnect(this)
+    }
+
+    fun setOnSignCallback(onSignCallback: OnSignCallback?) {
+        this.onSignCallback = onSignCallback
+    }
+
+    fun setOnConnectCallback(onConnectCallback: OnConnectCallback?) {
+        connectCallback = onConnectCallback
+    }
+
+    fun showConnectBottomDialog(
+        context: Context,
+        id: String,
+        website: String?,
+        iconUrl: String?,
+        walletInfo: BridgeMessageMetadata,
+        address: String
+    ) {
+        if (bottomSheetDialog != null && bottomSheetDialog!!.isShowing) {
+            bottomSheetDialog!!.dismiss()
+        }
+        bottomSheetDialog = BottomSheetDialog(context)
+        val view =
+            View.inflate(getContext(), R.layout.bottom_dialog_connect, null)
+        val btn_connect = view.findViewById<Button>(R.id.btn_connect)
+        val btn_cancel = view.findViewById<Button>(R.id.btn_cancel)
+        val tv_website_url = view.findViewById<TextView>(R.id.tv_website_url)
+        val iv_website_icon = view.findViewById<ImageView>(R.id.iv_website_icon)
+        val tv_address = view.findViewById<TextView>(R.id.tv_address)
+        if (website != null) {
+            tv_website_url.text = website
+        }
+        if (iconUrl != null) {
+            Glide.with(context).load(iconUrl).into(iv_website_icon)
+        }
+        tv_address.text = address
+        btn_connect.setOnClickListener {
+            DAppConnect.sendConnectResponse(id, address, true, walletInfo, false, null)
+            bottomSheetDialog!!.dismiss()
+            connectCallback?.connectApprove()
+        }
+        btn_cancel.setOnClickListener {
+            DAppConnect.sendConnectResponse(id, address, false, null, false, null)
+            bottomSheetDialog!!.dismiss()
+            connectCallback?.connectReject()
+        }
+        bottomSheetDialog!!.setContentView(view)
+        bottomSheetDialog!!.show()
+    }
+
+    fun showSignBottomDialog(
+        id: String,
+        participant: Participant,
+        address: String,
+        sign_content: String
+    ) {
+        if (bottomSheetDialog != null && bottomSheetDialog!!.isShowing) {
+            bottomSheetDialog!!.dismiss()
+        }
+        bottomSheetDialog = BottomSheetDialog(requireActivity())
+        val view = View.inflate(activity, R.layout.bottom_dialog_sign, null)
+        val btn_sign = view.findViewById<Button>(R.id.btn_sign)
+        val btn_cancel = view.findViewById<Button>(R.id.btn_cancel)
+        val tv_website_url = view.findViewById<TextView>(R.id.tv_website_url)
+        val iv_website_icon = view.findViewById<ImageView>(R.id.iv_website_icon)
+        val tv_address = view.findViewById<TextView>(R.id.tv_address)
+        val tv_sign_content = view.findViewById<TextView>(R.id.tv_sign_content)
+        if (participant.website != null) {
+            tv_website_url.setText(participant.website)
+        }
+        if (participant.iconUrl != null) {
+            Glide.with(activity).load(participant.iconUrl).into(iv_website_icon)
+        }
+        tv_address.text = address
+        tv_sign_content.text = sign_content
+        btn_sign.setOnClickListener {
+            if (onSignCallback != null) {
+                val signature: String = onSignCallback!!.sign(sign_content)!!
+                DAppConnect.sendSignResponse(
+                    id,
+                    true,
+                    signature,
+                    false,
+                    object : SendBridgeMessageCallback {
+                        override fun onReceived() {
+                            Log.i("SendBridgeMessageCallback","onReceived")
+//                            Toast.makeText(requireActivity(), "onReceived", Toast.LENGTH_SHORT).show()
+                        }
+
+                        override fun onFail() {
+                            Log.i("SendBridgeMessageCallback","onFail")
+//                            Toast.makeText(requireActivity(), "onFail", Toast.LENGTH_SHORT).show()
+                        }
+
+                        override fun onTimeout() {
+                            Log.i("SendBridgeMessageCallback","onTimeout")
+//                            Toast.makeText(requireActivity(), "onTimeout", Toast.LENGTH_SHORT).show()
+                        }
+                    })
+                bottomSheetDialog!!.dismiss()
+                onSignCallback!!.signApprove(participant.redirect)
+            }
+        }
+        btn_cancel.setOnClickListener {
+            if (onSignCallback != null) {
+                DAppConnect.sendSignResponse(id, false, null, false, null)
+                bottomSheetDialog!!.dismiss()
+                onSignCallback!!.signReject(participant.redirect)
+            }
+        }
+        bottomSheetDialog!!.setContentView(view)
+        bottomSheetDialog!!.show()
+    }
+
+    override fun onSuccess() {
+        DAppConnect.init(dAppID!!, object : BridgeConnectCallback {
+            override fun onConnectCallback() {
+                callback!!.initSuccess()
+            }
+
+            override fun onError(error: String) {
+                callback!!.onFail(error)
+            }
+        })
+        DAppConnect.setTargetTopicID(topicId!!)
+        DAppConnect.setTargetPubKey(pubKey!!)
+    }
+
+    override fun onFail(error: String) {
+        requireActivity().runOnUiThread {
+            cl_reconnect.visibility = View.VISIBLE
+            bottomSheetDialog?.dismiss()
+            callback?.onFail("connect websocket error:$error")
+        }
+    }
+
+    override fun alreadyConnected() {
+        DAppConnect.init(dAppID!!, object : BridgeConnectCallback {
+            override fun onConnectCallback() {
+                callback?.initSuccess()
+            }
+
+            override fun onError(error: String) {
+                callback?.onFail(error)
+            }
+        })
+        DAppConnect.setTargetTopicID(topicId!!)
+        DAppConnect.setTargetPubKey(pubKey!!)
+    }
+}
---
+++ wallet_demo/src/main/java/com/ty/dappconnect_kt/MainActivity.kt
@@ -0,0 +1,233 @@
+package com.ty.dappconnect_kt
+
+import android.Manifest
+import android.content.Intent
+import android.net.Uri
+import android.os.Bundle
+import android.os.Handler
+import android.text.TextUtils
+import android.util.Log
+import android.widget.CompoundButton
+import android.widget.ImageView
+import android.widget.Toast
+import android.widget.ToggleButton
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import com.ty.dappconnect.DAppConnect
+import com.ty.module_sign.fragment.WalletSignFragment
+import com.ty.module_sign.interfaces.OnConnectCallback
+import com.ty.module_sign.interfaces.OnSignCallback
+import com.ty.module_sign.interfaces.WalletInitCallback
+import com.ty.web3mq.Web3MQClient
+import com.ty.web3mq.WebsocketConfig
+import com.ty.web3mq.interfaces.OnSignRequestMessageCallback
+import com.ty.web3mq.utils.ConvertUtil
+import com.ty.web3mq.utils.CryptoUtils
+import com.ty.web3mq.utils.DefaultSPHelper
+import com.ty.web3mq.utils.RandomUtils
+import com.ty.web3mq.websocket.bean.BridgeMessageMetadata
+import com.ty.web3mq.websocket.bean.ConnectRequest
+import com.ty.web3mq.websocket.bean.sign.Participant
+import io.github.g00fy2.quickie.QRResult
+import io.github.g00fy2.quickie.ScanQRCode
+
+class MainActivity : AppCompatActivity() {
+    private var walletSignFragment: WalletSignFragment? = null
+    private val api_key = "rkkJARiziBQCscgg"
+    private val dAppID = "web3MQ_test_wallet:wallet"
+    private val ETH_ADDRESS = "0x0dBED6A18d4026B6B637AA9f89DE4a6aCc861D84"
+    private val ETH_PRV_KEY = "620db3575a3cd921e6c5dd91a9fce59573349f2f5dd81f84ae25e82c2275f4c2"
+    private lateinit var iv_scan: ImageView
+
+    //    private String handling_connect_uri = null;
+    private var btn_toggle: ToggleButton? = null
+    private var fromRemote = false
+    private var handle_url: String? = null
+    val scanQrCodeLauncher = registerForActivityResult(ScanQRCode()) { result ->
+        // handle QRResult
+        when(result){
+            is QRResult.QRSuccess -> {
+                fromRemote = true
+
+                handleConnectUri(Uri.parse(result.content.rawValue))
+            }
+            else -> {}
+        }
+
+    }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.CAMERA), 1)
+
+        Web3MQClient.init(this, api_key)
+        setContentView(R.layout.activity_main)
+        btn_toggle = findViewById(R.id.btn_toggle)
+        walletSignFragment = WalletSignFragment()
+        initView()
+        setListener()
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.add(R.id.fl_content, walletSignFragment!!).commitAllowingStateLoss()
+        DefaultSPHelper.clear()
+        btn_toggle!!.setOnCheckedChangeListener(CompoundButton.OnCheckedChangeListener { buttonView, isChecked ->
+            if (isChecked) {
+                Web3MQClient.switchUri(WebsocketConfig.WS_URL_TEST_NET)
+            } else {
+                Web3MQClient.switchUri(WebsocketConfig.WS_URL_DEV)
+            }
+        })
+
+    }
+
+    override fun onNewIntent(intent: Intent) {
+        super.onNewIntent(intent)
+        setIntent(intent)
+        fromRemote = true
+        Log.i(TAG, "onNewIntent")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.i(TAG, "onResume")
+        val uri = intent.data
+        if (uri != null) {
+            if(handle_url == null || handle_url!=uri.toString()) {
+                handleConnectUri(uri)
+                handle_url = uri.toString()
+            }
+        }
+    }
+
+    private fun initSignFragment(request: ConnectRequest) {
+        walletSignFragment!!.showLoadingDialog()
+        walletSignFragment!!.init(
+            dAppID,
+            request.topic!!,
+            request.publicKey!!,
+            object : WalletInitCallback{
+                override fun initSuccess() {
+                    val metaData = BridgeMessageMetadata()
+                    metaData.name = "Web3MQ Wallet"
+                    metaData.description = "ETH wallet"
+                    metaData.walletType = "eth"
+                    var icon: String? = null
+                    if (request.icons != null && request.icons!!.size > 0) {
+                        icon = request.icons!![0]
+                    }
+                    walletSignFragment!!.hideLoadingDialog()
+                    walletSignFragment!!.showConnectBottomDialog(
+                        this@MainActivity,
+                        request.id!!,
+                        request.url,
+                        icon,
+                        metaData,
+                        ETH_ADDRESS
+                    )
+                }
+
+                override fun onFail(error: String) {
+                    walletSignFragment!!.hideLoadingDialog()
+                    Toast.makeText(this@MainActivity, error, Toast.LENGTH_SHORT).show()
+                }
+            })
+    }
+
+    private fun handleConnectUri(uri: Uri?) {
+        Log.i(TAG,"handleConnectUri")
+        val request: ConnectRequest = ConvertUtil.convertDeepLinkToConnectRequest(uri.toString())
+        if (request.method != null && request.method.equals("provider_authorization")) {
+            DAppConnect.walletBuildAndSaveSession(request)
+            walletSignFragment!!.setOnConnectCallback(object : OnConnectCallback {
+                override fun connectApprove() {
+                    if (!fromRemote) {
+                        moveTaskToBack(true)
+                    }
+                }
+
+                override fun connectReject() {
+                    if (!fromRemote) {
+                        moveTaskToBack(true)
+                    }
+                    Log.i(TAG, "connectReject redirect:" + request.redirect)
+                }
+            })
+            DAppConnect.setOnSignRequestMessageCallback(object : OnSignRequestMessageCallback {
+                    override fun onSignRequestMessage(
+                        id: String,
+                        participant: Participant?,
+                        address: String,
+                        sign_raw: String
+                    ) {
+                        Log.i(TAG, "sign_raw: $sign_raw")
+                        if (TEST_MODEL) {
+                            Handler().postDelayed({
+                                if (RandomUtils.randomBoolean()) {
+                                    val signature: String =
+                                        CryptoUtils.signMessage(ETH_PRV_KEY, sign_raw)
+                                    DAppConnect.sendSignResponse(id, true, signature, false, null)
+                                    if (!TextUtils.isEmpty(request.redirect)) {
+                                        val intent =
+                                            Intent(Intent.ACTION_VIEW, Uri.parse(request.redirect))
+                                        startActivity(intent)
+                                    }
+                                } else {
+                                    DAppConnect.sendSignResponse(id, false, null, false, null)
+                                    if (!TextUtils.isEmpty(request.redirect)) {
+                                        val intent =
+                                            Intent(Intent.ACTION_VIEW, Uri.parse(request.redirect))
+                                        startActivity(intent)
+                                    }
+                                }
+                            }, 1000)
+                        } else {
+                            walletSignFragment!!.showSignBottomDialog(
+                                id,
+                                participant!!,
+                                address,
+                                sign_raw
+                            )
+                        }
+                    }
+                })
+            walletSignFragment!!.setOnSignCallback(object : OnSignCallback {
+                override fun sign(sign_raw: String): String {
+                    return CryptoUtils.signMessage(ETH_PRV_KEY, sign_raw)
+                }
+
+                override fun signApprove(redirect: String?) {
+                    if (!fromRemote) {
+                        moveTaskToBack(true)
+                    }
+                }
+
+                override fun signReject(redirect: String?) {
+                    if (!fromRemote) {
+                        moveTaskToBack(true)
+                    }
+                }
+            })
+            initSignFragment(request)
+        }
+    }
+
+    private fun initView() {
+        iv_scan = findViewById(R.id.iv_scan)
+    }
+
+    private fun setListener() {
+        iv_scan.setOnClickListener {
+            //scan
+            scanQrCodeLauncher.launch(null)
+        }
+    }
+
+    fun hideSignFragment() {
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.hide(walletSignFragment!!).commitAllowingStateLoss()
+    }
+
+    companion object {
+        private const val TAG = "MainActivity"
+        private const val TEST_MODEL = false
+    }
+}
